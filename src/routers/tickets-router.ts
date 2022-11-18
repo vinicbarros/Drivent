327cd914e8fd7139e9561d565824d09b0a3ba398
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-11-18 09:10:52 -0300

    feat: added post tickets route

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,9 +1,9 @@
-import { getTickets, getTicketsTypes } from "@/controllers/tickets-controller";
+import { getTickets, getTicketsTypes, postTickets } from "@/controllers/tickets-controller";
 import { authenticateToken } from "@/middlewares";
 import { Router } from "express";
 
 const ticketsRouter = Router();
 
-ticketsRouter.all("/*", authenticateToken).get("/types", getTicketsTypes).get("/", getTickets).post("/");
+ticketsRouter.all("/*", authenticateToken).get("/types", getTicketsTypes).get("/", getTickets).post("/", postTickets);
 
 export { ticketsRouter };
