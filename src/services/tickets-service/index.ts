5f577fc5ff9bb39d76798d11f25a564c6bf85c3a
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-11-18 09:14:30 -0300

    feat: added new functions to tickets service

--- src/services/tickets-service/index.ts
+++ src/services/tickets-service/index.ts
@@ -1,5 +1,5 @@
 import { notFoundError } from "@/errors";
-import ticketsRepository from "@/repositories/tickets-repository";
+import ticketsRepository from "@/repositories/ticket-repository";
 
 async function getTicketsTypes() {
   const ticketsTypes = await ticketsRepository.findTicketsTypes();
@@ -16,9 +16,23 @@ async function getTickets(userId: number) {
   return tickets;
 }
 
+async function getTicketTypeById(ticketId: number) {
+  const ticket = await ticketsRepository.findTicketTypeById(ticketId);
+
+  return ticket;
+}
+
+async function insertNewTicket(enrollmentId: number, ticketTypeId: number) {
+  const newTicket = await ticketsRepository.createNewTicket(enrollmentId, ticketTypeId);
+
+  return newTicket;
+}
+
 const ticketsService = {
   getTicketsTypes,
   getTickets,
+  getTicketTypeById,
+  insertNewTicket,
 };
 
 export default ticketsService;
