713ffb34d12aa57f79f3720d5ac470e2d969715a
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-11-18 09:12:13 -0300

    feat: created new postTickets function

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,4 +1,5 @@
 import { AuthenticatedRequest } from "@/middlewares";
+import enrollmentsService from "@/services/enrollments-service";
 import ticketsService from "@/services/tickets-service";
 import { Response } from "express";
 import httpStatus from "http-status";
@@ -19,6 +20,30 @@ export async function getTickets(req: AuthenticatedRequest, res: Response) {
   try {
     const tickets = await ticketsService.getTickets(userId);
     return res.status(httpStatus.OK).send(tickets);
+  } catch (error) {
+    if (error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
+
+export async function postTickets(req: AuthenticatedRequest, res: Response) {
+  const { ticketTypeId } = req.body;
+  const { userId } = req;
+
+  try {
+    const enrollmentId = (await enrollmentsService.getOneWithAddressByUserId(userId)).id;
+    const ticketType = await ticketsService.getTicketTypeById(ticketTypeId);
+    const newTicket = await ticketsService.insertNewTicket(enrollmentId, ticketTypeId);
+
+    return res.status(httpStatus.CREATED).send({
+      id: newTicket.id,
+      status: newTicket.status,
+      ticketTypeId,
+      enrollmentId,
+      TicketType: ticketType,
+      createdAt: newTicket.createdAt,
+      updatedAt: newTicket.updatedAt,
+    });
   } catch (error) {
     if (error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
     return res.sendStatus(httpStatus.BAD_REQUEST);
