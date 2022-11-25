68002bf885ac9e05641217e998a7c13e387157a0
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-11-25 11:18:50 -0300

    feat: created hotels get functions

---
+++ src/controllers/hotels-controller.ts
@@ -0,0 +1,33 @@
+import { AuthenticatedRequest } from "@/middlewares";
+import hotelsService from "@/services/hotels-service";
+import { Response } from "express";
+import httpStatus from "http-status";
+
+export async function getHotels(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+
+  try {
+    const hotels = await hotelsService.getHotels(userId);
+    return res.status(httpStatus.OK).send(hotels);
+  } catch (error) {
+    if (error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
+    if (error.name === "UnauthorizedError") return res.sendStatus(httpStatus.UNAUTHORIZED);
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
+
+export async function getRooms(req: AuthenticatedRequest, res: Response) {
+  const { hotelId } = req.params;
+  const { userId } = req;
+
+  try {
+    const rooms = await hotelsService.getRooms(Number(hotelId), userId);
+
+    return res.status(httpStatus.OK).send(rooms);
+  } catch (error) {
+    if (error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
+    if (error.name === "BadRequestError") return res.sendStatus(httpStatus.BAD_REQUEST);
+    if (error.name === "UnauthorizedError") return res.sendStatus(httpStatus.UNAUTHORIZED);
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
