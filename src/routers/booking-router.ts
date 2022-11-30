281b58176765bfbfea842a76e07b6fff3d488bd8
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-11-30 10:48:03 -0300

    docs: booking router

---
+++ src/routers/booking-router.ts
@@ -0,0 +1,9 @@
+import { getBooking, postBooking } from "@/controllers/booking-controller";
+import { authenticateToken } from "@/middlewares";
+import { Router } from "express";
+
+const bookingRouter = Router();
+
+bookingRouter.all("/*", authenticateToken).get("/", getBooking).post("/", postBooking);
+
+export { bookingRouter };
